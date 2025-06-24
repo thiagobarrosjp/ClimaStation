608debe6b1465c68ab61322d1b4f3ab69a7e1344
thiagobarrosjp <thiagobarrosjp@gmail.com>
2025-06-24 21:30:24 +0200

    Fix router import, enable working test route, update .gitignore

--- app/main.py
+++ app/main.py
@@ -1,31 +1,14 @@
-# Import the FastAPI class from fastapi library.
-# This class is what we'll use to create our web application.
 from fastapi import FastAPI
+from app.features.dwd.router import router as dwd_router
 
-# Import the router object that contains your API endpoints
-from app.api.routes import router
+app = FastAPI(
+    title="ClimaStation API",
+    description="Backend API to parse, analyze, and visualize climate data from DWD and beyond.",
+    version="0.1.0"
+)
 
+app.include_router(dwd_router, prefix="/dwd", tags=["DWD"])
 
-# Create an instance of the FastAPI app.
-# Think of this like the brain of your app. It handles incoming requests and sends responsed.
-app = FastAPI()
-
-# Register the router with the app (attaches all defined routes)
-app.include_router(router)
-
-
-
-# This line defines a route, a URL path that users or programs can call.
-# The @ symbol is a decorator, which means it modifies the function directly below it.
-# In this case, it tells FastAPI: "If someone accesses the root URL '/', run the function below."
 @app.get("/")
-
 def read_root():
-    # This is what will be returned when someone visits the root URL. 
-    # It returns a python dictionary, which FastAPI automatically converts to JSON for the web.
-    
     return {"ClimaStation": "Backend is running!"}
-
-
-
-
